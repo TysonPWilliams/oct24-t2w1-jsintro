1af1639d5330b2cb1b0f5494c345c63e4c70085e
TysonPWilliams <tyson.williams95@gmail.com>
2025-04-02 20:35:54 +1100

    Finished JS Intro

--- script.js
+++ script.js
@@ -53,3 +53,60 @@ console.log(Boolean({}))
 console.log(typeof('Hi'))
 console.log(typeof(21.4))
 console.log(typeof(2))
+
+console.log(1/2)
+console.log(1/"2") // Type coercion
+console.log(1/0)  //  = Infinity
+console.log(1/"hi") // = NaN (Not a Number)
+
+// Explicit Cast
+x = "2"
+console.log(1/Number(x))
+
+// Array (list in Python)
+const names = ['John', 'Jane', 'Bob']
+const foo = ['Matt', 52, 184.5]
+
+console.log(names)
+console.log(names[1])
+names[4] = 'Mary'
+console.log(names)
+names.push('Harry', 'Max') // Adds the new values to the end of the array
+console.log(names)
+const name = names.pop() // Remove the last item in an array and assign it to a variable
+console.log(names)
+console.log(name)
+// Python naming convention is snake case - big_array
+const bigArray = names.concat(foo) // Concatenates the two arrays and reassings them to a new variable name
+console.log(bigArray)
+console.log(names)
+
+
+// Objects - dict in Python
+const person1 = {
+    //Python: "name": "John"
+    name: "John",
+    age: 32,
+    city: "Sydney"
+}
+console.log(person1.age)
+// Object.freeze(person1)
+person1.country = "Australia"
+console.log(person1)
+
+const person2 = {
+    name: "Sally",
+    age: 45,
+    address: {
+        city: "Melbourne",
+        country: "Australia"
+    }
+}
+console.log(person2)
+console.log(person2.address.city)
+console.log(person2.address.country)
+
+// const attr = prompt('Which attribute?')
+// console.log(person2.address[attr])
+// Optional chaining with ?
+console.log(person1.address?.country)
